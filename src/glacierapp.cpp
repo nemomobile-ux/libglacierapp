feb122a54c6164b27ab141ba823feaba2d75e7e1
Chupligin Sergey <neochapay@gmail.com>
2020-04-05 10:51:51 +0300

    [WindowMode] Allow force window mode

--- src/glacierapp.cpp
+++ src/glacierapp.cpp
@@ -89,13 +89,20 @@ QQuickWindow *GlacierApp::showWindow()
         qCritical() << "Top object is not Window!";
         return nullptr;
     }
+//Add GLACIER_FORCE_WINDOW_MODE=1 into /var/lib/compositor/*.conf to run all app in window mode
+    bool forceWindowMode = false;
+    if(qgetenv("GLACIER_FORCE_WINDOW_MODE") == "1"){
+        forceWindowMode = true;
+    }
 
     if(QCoreApplication::arguments().contains("--prestart") || QCoreApplication::arguments().contains("-p"))
     {
         qDebug() << "Application run in shadow mode";
     }
     else{
-        if (QCoreApplication::arguments().contains("--window") || QCoreApplication::arguments().contains("-w"))
+        if (QCoreApplication::arguments().contains("--window")
+                || QCoreApplication::arguments().contains("-w")
+                || forceWindowMode)
         {
             /*Load last params of window*/
 #ifdef HAS_MLITE5
